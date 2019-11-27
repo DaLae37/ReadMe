14fd4160ed9eeb9bcc9be24a1e5c3fb4f8137d0a
DaLae37 <dalae37@gmail.com>
2019-11-27 18:34:29 +0900

    Realize Changing Acitivty

--- ReadMe/app/src/main/java/com/dalae37/readme/SummaryActivity.kt
+++ ReadMe/app/src/main/java/com/dalae37/readme/SummaryActivity.kt
@@ -4,21 +4,18 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Debug
-import android.util.Log
-import android.view.GestureDetector
 import android.view.MotionEvent
 
-class MainActivity : AppCompatActivity() {
+class SummaryActivity : AppCompatActivity() {
 
     private var mediaPlayer : MediaPlayer = MediaPlayer()
     private var curX : Float = 0f
-    private var curY : Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mediaPlayer = MediaPlayer.create(this, R.raw.main_view)
+        setContentView(R.layout.activity_summary)
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.summary_view)
         mediaPlayer.start()
     }
 
@@ -29,24 +26,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        when (event.action) {
+        when(event.action) {
             MotionEvent.ACTION_DOWN -> {
                 curX = event.x
-                curY = event.y
             }
             MotionEvent.ACTION_UP -> {
                 var diffX: Float = curX - event.x
-                var diffY: Float = curY - event.y
-                if (diffY > 600) {
-                    val intent = Intent(this, CameraActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else if (diffX < -300) {
-                    val intent = Intent(this, ResultActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else if (diffX > 300) {
-                    val intent = Intent(this, SummaryActivity::class.java)
+
+                if (diffX < -300) {
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
                 }
